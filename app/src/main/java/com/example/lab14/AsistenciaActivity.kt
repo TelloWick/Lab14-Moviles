3198b694b2656c61851c2300c77ade5a7842d55a
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-22 16:52:36 -0500

    Ejercicio Final - Creacion de archivos ClientesActivity.kt ,MembresiasActivity.kt y AsistenciaActivity.kt con sus respectivos codigos

---
+++ app/src/main/java/com/example/lab14/AsistenciaActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lab14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class AsistenciaActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            Text("Pantalla Asistencia")
+
+        }
+
+    }
+
+}
