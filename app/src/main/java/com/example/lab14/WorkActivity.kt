d5347816abd2bade5c0d7923342848bcb0862f28
TelloWick <fabriciovilca_2001@hotmail.com>
2026-06-22 16:35:35 -0500

    Creacion de archivo WorkActivity.kt

---
+++ app/src/main/java/com/example/lab14/WorkActivity.kt
@@ -0,0 +1,16 @@
+package com.example.lab14
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class WorkActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text(text = "Esta es la segunda vista: Work")
+        }
+    }
+}
